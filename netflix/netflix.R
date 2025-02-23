348e7df7a2ee2556217599415b5df6d3f98a40e9
Liam Cottrell <lemcottrell@gmail.com>
2025-02-23 20:42:12 +0000

    start some basic eda

---
+++ netflix/netflix.R
@@ -0,0 +1,18 @@
+# setup stuff
+library(tidyverse)
+library(janitor)
+netflix <- read_csv('private-ViewingActivity.csv')
+
+# rename variables - auto
+netflix <- clean_names(netflix)
+
+# rename variables - manual
+netflix <- netflix %>%
+  rename(type = supplemental_video_type)
+
+# frequency that each device was used - a bad bar chart to practice ggplot
+device_count <- as.data.frame(table(netflix$device_type))
+
+ggplot(device_count, aes(y = Var1, x = Freq)) +
+  geom_bar(stat = "identity", fill = "lightgreen", color = "black") +
+  labs(y = 'Device')
