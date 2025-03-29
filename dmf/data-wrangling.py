7481f15a860b715ae555cdef75f810f805c2c647
Liam Cottrell <lemcottrell@gmail.com>
2025-03-29 23:09:38 +0000

    tidy columns

--- dmf/data-wrangling.py
+++ dmf/data-wrangling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pyjanitor
 
 #read in csv
 df = pd.read_csv('./Air_Quality_Continuous.csv')
@@ -7,19 +8,29 @@ df = pd.read_csv('./Air_Quality_Continuous.csv')
 #sort by timestamp
 df = df.sort_values(by=['Date_Time'], ascending=True)
 
-#display first 5 rows
-df.head()
-
 #check for empty variables
 print(df.isnull().all())
 
 #check if objectid2 is unique
 print(df['ObjectId2'].is_unique)
 
-#remove objectid
-df = df.drop('ObjectId', axis=1)
+#clean columns
+df = (
+    #remove objectid
+    df.drop('ObjectId', axis=1)
+    #make objectid into primary key
+    .rename(columns={'ObjectId2':'id'})
+    #reorder columns
+    .reorder_columns(['id'])
+    #rename columns
+    .clean_names()
+    .rename(columns=lambda col: f"r_{col}")
+)
 
 #check for missing rows
 num_rows = df.shape[0]
 if num_rows == 1603492:
     print("No rows missing")
+
+#check dataframe
+df.head()
