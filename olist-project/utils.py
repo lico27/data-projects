7703dcb4352e613076071a36a4c379d941a2e569
Liam <liam@liamco.io>
2026-06-21 19:36:26 +0100

    do 1.8 and update function

--- olist-project/utils.py
+++ olist-project/utils.py
@@ -1,9 +1,9 @@
 
 
-def count_docs(db, collection, doc_name):
+def count_docs(db, collection, doc_name, operator, conditions):
     """
-    Counts the missing values in a given collection and calculates the proportion of the entire collection that these documents represent.
+    Counts the specified values in a given collection, based on given conditions, and calculates the proportion of the entire collection that these documents represent.
     """
-    doc_count = db[collection].count_documents({"$or": [{doc_name: {"$in": [None, ""]}}, {doc_name: {"$exists": False}}]})
+    doc_count = db[collection].count_documents({operator: [{doc_name: conditions[0]}, {doc_name: conditions[1]}]})
     proportion = doc_count / db[collection].count_documents({})
     return doc_count, proportion
